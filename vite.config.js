dad58d8b1b0b302f5bdd92e1470a4b32545ca9eb
frantz23 <ogoufrantz@gmail.com>
2026-07-19 15:44:33 +0100

    Add organization and pannel css file for vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/home.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/home.css', 'resources/css/organization.css', 'resources/css/pannel.css'],
             refresh: true,
         }),
     ],
