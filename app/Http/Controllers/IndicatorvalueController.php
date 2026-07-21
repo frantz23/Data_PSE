b748d8bd3424a434c11cf9a07bcc690e0e3e31fc
frantz23 <ogoufrantz@gmail.com>
2026-07-21 05:37:03 +0100

    indcatorValue link third try

--- app/Http/Controllers/IndicatorvalueController.php
+++ app/Http/Controllers/IndicatorvalueController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\IndicatorvalueFormRequest;
 use App\Models\Indicator;
+use App\Models\IndicatorValue;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
